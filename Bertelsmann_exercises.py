83ff61fca40e36bbdcbd55c3d7ba0798a1d17d54
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-07 19:06:51 +0100

    Bertelsmann scholarship Python exercises, rotating working file: create an HTML list

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,8 +1,27 @@
-tokens = ['<greeting>', 'Hello World!', '</greeting>']
-count = 0
+'''
+Create an HTML List
 
-for token in tokens:
-    if token[0] == '<' and token[-1] == '>':
-        count += 1
+Write some code, including a for loop, that iterates over a list of strings and creates a single string,
+html_str, which is an HTML list.
+For example, if the list is items = ['first string', 'second string'], printing html_str should output:
 
-print(count)
+<ul>
+<li>first string</li>
+<li>second string</li>
+</ul>
+
+That is, the string's first line should be the opening tag <ul>.
+Following that is one line per element in the source list, surrounded by <li> and </li> tags.
+The final line of the string should be the closing tag </ul>.
+
+'''
+
+items = ['first string', 'second string']
+html_str = "<ul>\n"
+
+for item in items:
+    html_str += '<li>' + item + '</li>\n'
+
+html_str += "</ul>\n"
+
+print(html_str)
