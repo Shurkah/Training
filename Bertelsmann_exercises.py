7e3657534924a671aaf7dc42d58653a3e9361cb6
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-09 21:40:17 +0100

    Bertelsmann scholarship Python exercises, rotating working file: fruits and not fruits

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,14 +1,16 @@
-# You would like to count the number of fruits in your basket. 
+# You would like to count the number of fruits in your basket.
 # In order to do this, you have the following dictionary and list of
 # fruits.  Use the dictionary and list to count the total number
-# of fruits, but you do not want to count the other items in your basket.
+# of fruits and not_fruits.
 
-result = 0
+fruit_count, not_fruit_count = 0, 0
 basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
 fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
 
-for fruit in fruits:
-    if fruit in basket_items:
-        result += basket_items[fruit]
+for key, value in basket_items.items():
+    if key in fruits:
+        fruit_count += value
+    else:
+        not_fruit_count += value
 
-print(result)
+print(fruit_count, not_fruit_count)
