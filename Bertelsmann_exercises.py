b345695855ea1232a6e8c6319d51f9c9f67f7edd
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-10 20:56:57 +0100

    Bertelsmann scholarship Python exercises, rotating working file: while cby

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,23 +1,16 @@
-# number to find the factorial of
-number = 6
-# start with our product equal to one
-product = 1
-# track the current number being multiplied
-current = 1
+start_num = 5 #provide some start number, replace 5 with a number you choose
+end_num = 100#provide some end number that you stop when you hit, replace 100 with a number you choose
+count_by = 2 #provide some number to count by, replace 2 with a number you choose 
 
-while current <= number:
-    product *= current
-    current += 1
+# write a while loop that uses break_num as the ongoing number to 
+# check against end_num
+break_num = start_num
+result = "Oops!  Looks like your start value is greater than the end value.  Please try again."
+if start_num > end_num:
+    print(result)
+else:
+    while break_num < end_num:
+        break_num += count_by
+        result = break_num
 
-print(product)
 
-
-# number to find the factorial of
-number = 6
-# start with our product equal to one
-product = 1
-
-for i in range(1, number + 1):
-    product *= i
-
-print(product)
