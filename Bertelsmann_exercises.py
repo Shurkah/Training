593925ba01e37e0cd66e609156fa75952d8d74c2
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-11 20:14:02 +0100

    Bertelsmann scholarship Python exercises, rotating working file: news ticker

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,15 +1,18 @@
-start_num = 5 #provide some start number, replace 5 with a number you choose
-end_num = 100#provide some end number that you stop when you hit, replace 100 with a number you choose
-count_by = 2 #provide some number to count by, replace 2 with a number you choose 
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
 
-# write a while loop that uses break_num as the ongoing number to 
-# check against end_num
-break_num = start_num
-result = "Oops!  Looks like your start value is greater than the end value.  Please try again."
-if start_num > end_num:
-    print(result)
-else:
-    while break_num < end_num:
-        break_num += count_by
-        result = break_num
+news_ticker = ""
 
+for headline in headlines:
+    news_ticker += headline + ' '
+    if len(news_ticker) >= 140:
+        break
+
+news_ticker = news_ticker[:140]
+
+print(news_ticker)
+print(len(news_ticker))
