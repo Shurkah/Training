6de22e1eedfd9a10988f88b6aeabd7953611f293
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-12 23:22:20 +0100

    Bertelsmann scholarship Python exercises: prime numbers, timed

--- randomnumbers.py
+++ randomnumbers.py
@@ -2,7 +2,7 @@ import random
 
 least = []
 while len(least) < 20:
-    a = random.randint(10 ** 2, 10 ** 5)
+    a = random.randint(10 ** 2, 10 ** 7)
     if a % 2 != 0 and a % 3 != 0:
         least.append(a)
 
