734b6502cffa6ee5f538fad92be311b9df20e19d
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-13 22:15:36 +0100

    Bertelsmann scholarship Python exercises: dict zip

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,11 +1,5 @@
-x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
-y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
-z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
-labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
 
-points = []
-
-for label, x, y, z in zip(labels, x_coord, y_coord, z_coord):
-    points.append('{}: {}, {}, {}'.format(label, x, y, z))
-
-print(points)
+cast = dict(zip(cast_names, cast_heights))
+print(cast)
