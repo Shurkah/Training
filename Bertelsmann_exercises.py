6b9b30a136f4ab1ac01364d097b50a39135df4c4
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-13 22:17:11 +0100

    Bertelsmann scholarship Python exercises: unzip

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,5 +1,5 @@
-cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
-cast_heights = [72, 68, 72, 66, 76]
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
 
-cast = dict(zip(cast_names, cast_heights))
-print(cast)
+
+names, heights = zip(*cast)
+print(names, heights)
