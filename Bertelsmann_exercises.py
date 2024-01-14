5dfb4368047adfee5309f8afef96cc4f49211c98
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-14 14:59:00 +0100

    Bertelsmann scholarship Python exercises: passed scores

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,2 +1,10 @@
-multiples_3 = [3 * i for i in range(1, 21)]
-print(multiples_3)
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [key for key, value in scores.items() if value >= 65]
+print(passed)
