25a85eb0c3b1d2d511c635ec1a2039935eb255cd
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-15 21:10:02 +0100

    Bertelsmann scholarship, rotating exercises file: readable_timedelta

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,10 +1,9 @@
-scores = {
-             "Rick Sanchez": 70,
-             "Morty Smith": 35,
-             "Summer Smith": 82,
-             "Jerry Smith": 23,
-             "Beth Smith": 98
-          }
+# Write a function named readable_timedelta.
+# The function should take one argument, an integer days, and return a string that says how many weeks and days that is.
 
-passed = [key for key, value in scores.items() if value >= 65]
-print(passed)
+def readable_timedelta(days):
+    weeks = int(days / 7)
+    leftover_days = int (days % 7)
+    return '{} week(s) and {} day(s).'.format(weeks, leftover_days)
+
+print(readable_timedelta(10))
