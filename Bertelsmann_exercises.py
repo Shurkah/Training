ecb39831c3f3dd1696b85811f26388a07f692df1
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-15 22:36:15 +0100

    Bertelsmann scholarship rotating exercises file: Lambda with Map

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -1,9 +1,14 @@
-# Write a function named readable_timedelta.
-# The function should take one argument, an integer days, and return a string that says how many weeks and days that is.
+numbers = [
+              [34, 63, 88, 71, 29],
+              [90, 78, 51, 27, 45],
+              [63, 37, 85, 46, 22],
+              [51, 22, 34, 11, 18]
+           ]
 
-def readable_timedelta(days):
-    weeks = days // 7
-    leftover_days = days % 7
-    return '{} week(s) and {} day(s).'.format(weeks, leftover_days)
+# def mean(num_list):
+#     return sum(num_list) / len(num_list)
 
-print(readable_timedelta(10))
+averages = list(map(mean, numbers))
+
+averages = list(map(lambda x: sum(x) / len(x), numbers))
+print(averages)
