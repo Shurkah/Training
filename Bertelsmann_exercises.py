14c2ac9f024da17ff9aa5e445b5917be88c667eb
Aleksandra Bunina <a.bun.in.a.box@gmail.com>
2024-01-15 21:26:29 +0100

    Bertelsmann scholarship, rotating exercises file: readable_timedelta, better formatting

--- Bertelsmann_exercises.py
+++ Bertelsmann_exercises.py
@@ -2,8 +2,8 @@
 # The function should take one argument, an integer days, and return a string that says how many weeks and days that is.
 
 def readable_timedelta(days):
-    weeks = int(days / 7)
-    leftover_days = int (days % 7)
+    weeks = days // 7
+    leftover_days = days % 7
     return '{} week(s) and {} day(s).'.format(weeks, leftover_days)
 
 print(readable_timedelta(10))
